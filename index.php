592f2a717e4be49813c3fc6c412a0fbd5c687465
GroveEmpire <85416017+GroveEmpire@users.noreply.github.com>
2021-06-10 16:10:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+@$fp = fopen("ip_log.txt", "ab");
+if(!$fp) {
+echo "Could not open file for logging";
+}
+else {
+$str = $_SERVER["REMOTE_ADDR"];
+$str .= " ";
+$str .= date("H:i, d M Y");
+fwrite($fp, $str);
+fclose($fp);
+}
+
 header("Location: index.html"); 
 exit;
 ?>
